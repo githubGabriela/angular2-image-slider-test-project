3a02de930cbb7a8855dc4ef2ecc169ef5fe59e3d
githubGabriela <gabriela_balasel@yahoo.com>
2016-05-03 14:10:54 +0200

    addded images-uploader

---
+++ image-slider/src/app/components/upload-images/images-uploader.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+@Component ({
+  selector : 'upload-images',
+  directives : [],
+  templateUrl : 'app/components/upload-images/upload-images.html',
+  styleUrls : ['app/components/upload-images/upload-images.css']
+})
+
+export class UploadImages{
+  public constructor(){
+
+  }
+}
