0773a2028deae86e546eabc17e980a6acd53ca5a
githubGabriela <gabriela_balasel@yahoo.com>
2016-05-03 20:03:09 +0200

    update view images-loader

--- image-slider/src/app/components/uploader/uploader.ts
+++ image-slider/src/app/components/uploader/uploader.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgStyle} from 'angular2/common';
-import {FILE_UPLOAD_DIRECTIVES, FileSelect, FileDrop, FileUploader} from 'ng2-file-upload';
+import {FILE_UPLOAD_DIRECTIVES, FileUploader} from 'ng2-file-upload';
 
 const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
@@ -8,7 +8,8 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
   selector: 'uploader',
   templateUrl: 'app/components/uploader/uploader.html',
   styleUrls: ['app/components/uploader/uploader.css'],
-  directives: [FILE_UPLOAD_DIRECTIVES,FileSelect, FileDrop, NgClass, NgStyle, CORE_DIRECTIVES, FORM_DIRECTIVES]
+  directives: [FILE_UPLOAD_DIRECTIVES, NgClass, NgStyle, CORE_DIRECTIVES, FORM_DIRECTIVES],
+  pipes:[]
 })
 export class Uploader {
   private uploader:FileUploader = new FileUploader({url: URL});
@@ -23,4 +24,13 @@ export class Uploader {
    private fileOverAnother(e:any) {
      this.hasAnotherDropZoneOver = e;
    }
+
+   test (){
+     console.log('test');
+   }
+
+   onFileDrop(file: File) {
+     console.log('dropped');
+   //Do something with the file dropped
+ }
 }
